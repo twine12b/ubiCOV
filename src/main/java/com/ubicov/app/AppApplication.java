fb29375f4411a8fd9be8ce440208a292f3205708
aczx131 <richard.renaud@city.co.uk>
2021-04-04 10:54:16 +0100

    TDD of GeoLocation with full integration test compliance now completed.  Comments about the functionality and purpose of each class can be found at the head of each class.
    **TODO**
    - Merge the groups individual react components into this repository.
    - TDD furlough data structure / service / repository / controller.

---
+++ src/main/java/com/ubicov/app/AppApplication.java
@@ -0,0 +1,63 @@
+/**
+ * @Author - Richard Renaud
+ * Hard coding dummy data to test react async calls to spring boot.
+ * NB.  Remove before production release.
+ */
+package com.ubicov.app;
+
+import lombok.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")  // set to local React front end server port
+public class AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+
+    @RequestMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        System.out.println(name);
+        return String.format("Hello %s!", name);
+    }
+
+    @GetMapping("/suppliers")
+    public List supplier() {
+        List lst = new ArrayList<>();
+        lst.add(new Supplier(1L, "name1", "pic1"));
+        lst.add(new Supplier(2L, "name2", "pic2"));
+        lst.add(new Supplier(3L, "name3", "pic3"));
+
+        return lst;
+    }
+
+    @Entity
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @ToString
+    @EqualsAndHashCode
+    public class Supplier {
+        @Id
+        private long supplierId;
+        private String name;
+        private String logo;
+
+        public Supplier(long suppId, String name, String logo) {
+            this.name = name;
+            this.supplierId = suppId;
+            this.logo = logo;
+        }
+    }
+
+
+}
